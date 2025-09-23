aeb3902b15e75f2edb6222faad09eac4bceb9148
ahmed-elgaml11 <a7medelgaml11a@gmail.com>
2025-09-23 19:52:34 +0300

    add postgres

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -5,27 +5,38 @@ import { log } from 'console';
 import mongoose from 'mongoose'
 dotenv.config({path: path.join(__dirname, '../.env')});
 import { createClient } from "redis";
+import { Client } from 'pg'
 
-const redisClient = createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+
+const postgresUrl = `postgresql://root:example@postgres:5432`
+const client = new Client({
+    connectionString: postgresUrl
 })
-redisClient.on("error", (err) => console.log("Redis Client Error", err))
-redisClient.on('connect', () => log('connecting to redis...'))
-redisClient.connect();
+client
+.connect()
+.then(() => console.log('connected to postgres'))
+.catch(err => console.log(err)) 
+
+// const redisClient = createClient({
+//     url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+// })
+// redisClient.on("error", (err) => console.log("Redis Client Error", err))
+// redisClient.on('connect', () => log('connecting to redis...'))
+// redisClient.connect();
 
 
 const app = express()
-mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`)
-.then(() => {
-    log('connecting to db...')
-}).catch(err => {
-    log (err)
-})
+// mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`)
+// .then(() => {
+//     log('connecting to db...')
+// }).catch(err => {
+//     log (err)
+// })
 
-app.get('/', (req, res) => {
-    redisClient.set('products', 'products.....')
-    res.send('hi.....')
-})
+// app.get('/', (req, res) => {
+//     redisClient.set('products', 'products.....')
+//     res.send('hi.....')
+// })
 app.get('/data', async (req, res) => {
     // const data = await redisClient.get('products')
     res.json({
@@ -43,7 +54,7 @@ app.get('/hi', (req, res) => {
 
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log(`lestining on ${port}`);
 })
